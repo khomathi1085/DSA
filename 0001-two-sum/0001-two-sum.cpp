73217981a656f76cee93606227e5a55a23d28a3f
khomathi1085 <130474818+khomathi1085@users.noreply.github.com>
2024-11-02 16:29:22 +0530

    Time: 4 ms (66.02%), Space: 14.4 MB (36.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,11 +3,11 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> ans;
         for(int i=0;i<nums.size();i++){
-            if(ans.find(target-nums[i])==ans.end()){
-                ans[nums[i]]=i;
+            if(ans.find(target-nums[i])!=ans.end()){
+                return {i,ans[target-nums[i]]};
             }
             else{
-                return {i,ans[target-nums[i]]};
+                ans[nums[i]]=i;
             }
         }
         return {};
