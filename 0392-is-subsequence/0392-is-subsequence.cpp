6e5c7cd87860e07de1c8f5a991ff109441194e3d
khomathi1085 <130474818+khomathi1085@users.noreply.github.com>
2024-12-12 18:18:21 +0530

    Time: 0 ms (100%), Space: 8.6 MB (11.57%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,19 +3,15 @@ public:
     bool isSubsequence(string s, string t) {
         int i=0;
         int j=0;
-        while(i<s.size() && j<t.size()){
+        while(i<s.size()&&j<t.size()){
             if(s[i]==t[j]){
-                i++;j++;
+                i++;
+                j++;
             }
             else{
                 j++;
             }
         }
-        if(i==s.size()){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (i==s.size());
     }
 };
