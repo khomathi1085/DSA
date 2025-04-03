d33f8affcc5364541bb26de5832f1ea2fd72c9ef
khomathi1085 <130474818+khomathi1085@users.noreply.github.com>
2025-04-03 08:17:04 +0530

    Time: 0 ms (100%), Space: 12.2 MB (71.13%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -5,21 +5,21 @@ public:
         int j=n-1;
         int k=m+n-1;
         while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
+            if(nums2[j]>=nums1[i]){
+                nums1[k]=nums2[j];
                 k--;
+                j--;
             }
             else{
-                nums1[k]=nums2[j];
-                j--;
+                nums1[k]=nums1[i];
                 k--;
+                i--;
             }
         }
         while(j>=0){
             nums1[k]=nums2[j];
-            j--;
             k--;
+            j--;
         }
     }
 };
